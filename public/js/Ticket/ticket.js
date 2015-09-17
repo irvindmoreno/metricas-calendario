4ef417296ffaf8e99936ad659bfaae01e06d93d5
Irvin Moreno <daniel.moreno@clicksandbricks.pe>
2015-09-17 16:46:28 -0500

    Botones a,s,w,d funcionando perfectamente

---
+++ public/js/Ticket/ticket.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Ticket = function Ticket() //Ejemplo ("Enero",2015) (La primera letra con mayuscula)
+{
+	_classCallCheck(this, Ticket);
+};
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,35 @@
 "use strict";
 
-//var mes = new Mes("Febrero");
-//	console.log(mes.obtenerNumeroDeDiasEnElAnio(2016));
+$(document).on("ready", inicio);
+function inicio() {
+	$('.Mes-Cuadrito--white').on("keydown", escuchadorDeFlechas);
+}
+function escuchadorDeFlechas(event) {
+	var tabindex = $('*:focus').attr("tabindex");
+	tabindex = parseInt(tabindex);
+	switch (event.keyCode) {
+		case 87:
+			//arriba
+			tabindex = tabindex - sumaDeDiasEnUnTicket; //sumaDeDiasEnMeses => suma de Dias que hay en los meses del array
+			$('[tabindex=' + tabindex + ']').focus();
+			break;
+		case 83:
+			//abajo
+			tabindex = tabindex + sumaDeDiasEnUnTicket; //sumaDeDiasEnMeses => suma de Dias que hay en los meses del array
+			$('[tabindex=' + tabindex + ']').focus();
+			break;
+		case 65:
+			//izquierda
+			tabindex -= 1;
+			$('[tabindex=' + tabindex + ']').focus();
+			console.log("izquierda");
+			break;
+		case 68:
+			//derecha
+			tabindex += 1;
+			$('[tabindex=' + tabindex + ']').focus();
+			//$("#"+tabindex).focus();
+			console.log("derecha");
+			break;
+	}
+}
